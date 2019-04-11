386de3e473349c4a77934aa25d2606c403b70449
Scott Wagner <af17317596@gmail.com>
2019-04-11 12:11:15 -0500

    Update App.js

--- clickygame/src/components/App.js
+++ clickygame/src/components/App.js
@@ -9,8 +9,8 @@ export default App;
 // homework
 // search props for react
 // search state for react how to use state in react
-// search class based components
-// search function based components
+// search class based components in react
+// search function based components in react
 // what is the difference between the 2 components
 // define these 3
 // componentDidMount 
