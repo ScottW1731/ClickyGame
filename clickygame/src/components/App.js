df58cebf09f12554413321772c0bdad030026d7a
Scott Wagner <af17317596@gmail.com>
2019-04-10 19:17:11 -0500

    import react

---
+++ clickygame/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = ()=>{ 
+    return  <div> hello </div>    
+}
+
+export default App;
+
+// homework
+// search props for react
+// search state for react how to use state in react
+// search class based components
+// search function based components
+// what is the difference between the 2 components
+// define these 3
+// componentDidMount 
+// componentDidUpdate
+// componentWillUnmount
+// 
