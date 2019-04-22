06d4c56b18c07704529bc29cf06666801546164b
Scott Wagner <af17317596@gmail.com>
2019-04-22 01:27:56 -0500

    deploy to heroku
    trying to deploy to heroku  following instruction guide
    
    https://medium.com/jeremy-gottfrieds-tech-blog/tutorial-how-to-deploy-a-production-react-app-to-heroku-c4831dfcfa08

---
+++ clickygame/build/precache-manifest.85ec47637291ac4442f9b861ed78817f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69eb9148cffa0b1a9425",
+    "url": "/static/js/main.0d2fc05b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "fd4913f9d5884d796cf0ddd429e3cc16",
+    "url": "/index.html"
+  }
+];
